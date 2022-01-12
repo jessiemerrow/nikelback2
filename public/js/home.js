1b41bcc33f8d77c532acf85b4f322e259bb65737
Jana <41707901+jessiemerrow@users.noreply.github.com>
2022-01-11 23:02:45 -0300

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,85 +1,168 @@
-const myModal = new bootstrap.Modal("#transaction-modal")
-let logged = sessionStorage.getItem("logged");
-const session = localStorage.getItem('session')
-let cashIn = []
-let cashOut = []
+const myModal = new bootstrap.Modal("#transaction-modal");
+const session = localStorage.getItem("session");
+let  logged = sessionStorage.getItem("logged");
 let data = {
     transactions: []
 }
 
-document.getElementById('button-logout').addEventListener('click', logout)
+document.getElementById("button-logout").addEventListener("click", logout)
+document.getElementById("button-transaction").addEventListener("click", function() {
+    window.location.href = "transactions.html";
+});
 
-//adicionar lançamento
-document.getElementById('transaction-form').addEventListener('submit', function(e){
-    e.preventDefault()
-    const value = parseFloat(document.getElementById('value-input').value);
-    const description = document.getElementById('description-input').value;
-    const date = document.getElementById('description-input').value;
+// Adicionar lançamento
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
     const type = document.querySelector('input[name="type-input"]:checked').value;
 
     data.transactions.unshift({
-        value: value, type:type, description: description, date: date
+        value: value,
+        type: type,
+        description: description,
+        date: date
     })
 
     saveData(data);
-    e.target.reset()
-    myModal.hide
-    alert('Lanamento adicionado com sucesso')
+    e.target.reset();
+    myModal.hide();
 
-
-    
-})
+    getCashIn();
+    getCashOut();
+    getTotal();
+    alert("Lançamento adicionado com sucesso.");
+});
 
 checkLogged();
 
-function checkLogged(){
-    if(session){
-        sessionStorage.setItem('logged', session)
+function checkLogged() {
+    if (session) {
+        sessionStorage.setItem("logged", session);
         logged = session;
     }
-    if(!logged){
-        saveSession(logged, session)
-        window.location.href = 'index.html'
-        return
+
+    if (!logged) {
+        window.location.href = "index.html";
     }
 
-    const dataUser = localStorage.getItem(logged)
-    if(dataUser){
-        data = JSON.parse(dataUser)
-
+    const dataUser = localStorage.getItem(logged);
+    if (dataUser) {
+        data = JSON.parse(dataUser);
     }
 
     getCashIn();
+    getCashOut();
+    getTotal();
 }
 
-function logout(){
-    sessionStorage.removeItem('logged');
-    localStorage.removeItem('session')
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
 
-    window.location.href = 'index.html'
+    window.location.href = "index.html";
 }
 
-function getCashIn(){
-    const transactions = data.transactions
+function getCashIn() {
+    const transactions = data.transactions;
 
-    const cashIn = transactions.filter((item) => item.type ==="1")
+    const cashIn = transactions.filter((item) => item.type === "1");
 
-    if(cashIn.length){
+    if (cashIn.length) {
         let cashInHtml = ``;
-        let limit=0;
-    
-    if(cashIn.length > 5){
-        limit= 5;
-    }else{
-        limit = cashIn.length
+        let limit = 0;
+
+        if (cashIn.length > 5) {
+            limit = 5;
+        }
+        else {
+            limit = cashIn.length
+        }
+
+        for (let i = 0; i < limit; i++) {
+            cashInHtml += `
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h3 class="fs-2">R$ ${cashIn[i].value.toFixed(2)}</h3>
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-12 col-md-8">
+                                <p>${cashIn[i].description}</p>
+                            </div>
+                            <div class="col-12 col-md-3 d-flex justify-content-end">
+                                ${cashIn[i].date}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("cash-in-list").innerHTML = cashInHtml;
     }
-    for (let index = 0; index < array.length; index++) {
-        cashInHtml
-        
-    }
-}
 }
 
-function saveData(){
-    localStorage.setItem(data.login, JSON.stringify(data))
+function getCashOut() {
+    const transactions = data.transactions;
+
+    const cashIn = transactions.filter((item) => item.type === "2");
+
+    if (cashIn.length) {
+        let cashInHtml = ``;
+        let limit = 0;
+
+        if (cashIn.length > 5) {
+            limit = 5;
+        }
+        else {
+            limit = cashIn.length
+        }
+
+        for (let i = 0; i < limit; i++) {
+            cashInHtml += `
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h3 class="fs-2">R$ ${cashIn[i].value.toFixed(2)}</h3>
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-12 col-md-8">
+                                <p>${cashIn[i].description}</p>
+                            </div>
+                            <div class="col-12 col-md-3 d-flex justify-content-end">
+                                ${cashIn[i].date}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("cash-out-list").innerHTML = cashInHtml;
+    }
+}
+
+function getTotal() {
+    const transaction = data.transactions;
+    let total = 0;
+
+    transaction.forEach((item) => {
+        if (item.type === "1")
+        {
+            total += item.value;
+        }
+        else
+        {
+            total -= item.value;
+        }
+    });
+
+    document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
 }
